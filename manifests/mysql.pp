7a1a39af2bb77f8d52a84995c4548ea0d8276baf
Clement Parisot <clement.parisot@uni.lu>
2017-07-03 10:09:12 +0200

    Update mysql default parameter, update cacti to version 1.1.10

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -2,11 +2,27 @@
 #
 # This class is called from cacti for database config.
 # git@github.com:puppetlabs/puppetlabs-mysql.git
-class cacti::mysql inherits ::cacti{
+class cacti::mysql(
+  $override_options = {
+    'mysqld' => {
+      'max_heap_table_size' => Integer($::memory['system']['total_bytes'] * 0.10),
+      'max_allowed_packet'  => '16M',
+      'tmp_table_size'      => '64M',
+      'join_buffer_size'    => '64M',
+      'innodb_file_per_table' => 'ON',
+      'innodb_buffer_pool_size' => Integer($::memory['system']['total_bytes'] * 0.25),
+      'innodb_doublewrite' => 'OFF',
+      'innodb_additional_mem_pool_size' => '80M',
+      'innodb_lock_wait_timeout' => '50',
+      'innodb_flush_log_at_trx_commit' => '2'
+      }
+    },
+  ) inherits ::cacti{
 
   class { '::mysql::server':
     root_password           => $::cacti::database_root_pass,
     remove_default_accounts => true,
+    override_options => $override_options,
   }
 
   mysql::db { 'cacti':
@@ -14,7 +30,15 @@ class cacti::mysql inherits ::cacti{
     password => $::cacti::database_pass,
     host     => $::cacti::database_host,
     grant    => ['ALL'],
-    sql      => '/usr/share/doc/cacti-0.8.8b/cacti.sql',
+    sql      => '/usr/share/doc/cacti-1.1.10/cacti.sql',
+    charset => 'utf8',
+    collate => 'utf8_general_ci',
+    notify => Exec['patch MySQL TimeZone support'],
+  }
+
+  exec { 'patch MySQL TimeZone support':
+    command => "mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -Dmysql -p$::cacti::database_root_pass && mysql -p$::cacti::database_root_pass -D mysql -e \"GRANT SELECT ON mysql.time_zone_name TO ${::cacti::database_user}@localhost; flush privileges;\"",
+    refreshonly => true;
   }
 
 }
